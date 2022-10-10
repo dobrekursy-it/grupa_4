545dde67aa57add9de9add4f63c384ac5ae4fea8
Seweryn Jagusiak <jagusiak@gmail.com>
2022-10-10 19:55:02 +0200

    Add files

--- automatyczne/automatyczne/features/kolko_i_krzyzyk.feature
+++ automatyczne/automatyczne/features/kolko_i_krzyzyk.feature
@@ -1,1 +1,31 @@
 Feature: Kółko i krzyżyk
+  Scenario: Kiedy uzytkownik kliknie w kafalek, to zaznacza kolko
+    Given Uzytkownik uruchomil gre
+    When Uzytkownik kliknal w "gorny" "lewy" kafelek
+    Then "gorny" "lewy" kafelek jest "kolkiem"
+
+  Scenario: Kiedy uzytkownik kliknie w kolejny kafalek, to zaznacza krzyzyk
+    Given Uzytkownik uruchomil gre
+    When Uzytkownik kliknal w "gorny" "lewy" kafelek
+         And Uzytkownik kliknal w "srodkowy" "srodkowy" kafelek
+    Then "srodkowy" "srodkowy" kafelek jest "krzyzkiem"
+
+  Scenario: Kolko wygrywa
+    Given Uzytkownik uruchomil gre
+    When Uzytkownik kliknal w "gorny" "lewy" kafelek
+         And Uzytkownik kliknal w "srodkowy" "srodkowy" kafelek
+         And Uzytkownik kliknal w "srodkowy" "lewy" kafelek
+         And Uzytkownik kliknal w "gorny" "prawy" kafelek
+         And Uzytkownik kliknal w "dolny" "lewy" kafelek
+    Then "kolko" wygrywa
+
+
+  Scenario: Uruchomienie ponowne gry
+      Given Uzytkownik uruchomil gre
+      When Uzytkownik kliknal w "gorny" "lewy" kafelek
+           And Uzytkownik kliknal w "srodkowy" "srodkowy" kafelek
+           And Uzytkownik kliknal w "srodkowy" "lewy" kafelek
+           And Uzytkownik kliknal w "gorny" "prawy" kafelek
+           And Uzytkownik kliknal w "dolny" "lewy" kafelek
+           And Uzytwkonik kliknal w powtorz
+      Then uruchomiona nowa gra
--- automatyczne/automatyczne/features/step_definitions/stepdef.js
+++ automatyczne/automatyczne/features/step_definitions/stepdef.js
@@ -4,3 +4,39 @@ const {Builder, By} = require('selenium-webdriver');
 setDefaultTimeout(600 * 1000);
 let driver = new Builder().forBrowser('chrome').build();
 
+const pobierzKafelek = (rzad, kolumna) => {
+  const num = (["gorny", "srodkowy", "dolny"].indexOf(rzad) * 3) + ["lewy", "srodkowy", "prawy"].indexOf(kolumna);
+  return driver.findElement(By.id('s'+ num));
+};
+
+const pobierzZnak = (pelnaNazwaZnaku) => pelnaNazwaZnaku.startsWith("kol") ? "o" : "x";
+
+Given('Uzytkownik uruchomil gre', function () {
+  driver.get('https://jagusiak.github.io/xo/');
+});
+
+When('Uzytkownik kliknal w {string} {string} kafelek', function (rzad, kolumna) {
+    pobierzKafelek(rzad, kolumna).click();
+});
+
+When('Uzytwkonik kliknal w powtorz', function () {
+    driver.findElement(By.id('o-win')).findElement(By.className('link')).click();
+});
+
+Then('{string} {string} kafelek jest {string}', async function (rzad, kolumna, pelnaNazwaZnaku) {
+    const dataSign = await pobierzKafelek(rzad, kolumna).getAttribute('data-sign');
+    assert.strictEqual(pobierzZnak(pelnaNazwaZnaku), dataSign);
+});
+
+Then('{string} wygrywa', async function (pelnaNazwaZnaku) {
+    const isDisplayed = await driver.findElement(By.id(pobierzZnak(pelnaNazwaZnaku) + '-win')).isDisplayed();
+    assert.strictEqual(true, isDisplayed);
+});
+
+Then('uruchomiona nowa gra', async function () {
+  const opcje = ["o", "x", "d"];
+  for (let i = 0 ;  i< opcje.length; i++) {
+    const isDisplayed = await driver.findElement(By.id(opcje[i]+ '-win')).isDisplayed();
+    assert.strictEqual(false, isDisplayed);
+  }
+});
